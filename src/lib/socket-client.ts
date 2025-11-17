9df9ca8e30522ad15e8333fb361e350ea191c76d
Junk Truong <junk.truongg@gmail.com>
2025-11-17 17:03:36 +0700

    first second

--- src/lib/socket-client.ts
+++ src/lib/socket-client.ts
@@ -4,7 +4,7 @@ let socket: Socket | null = null;
 
 export function getSocket() {
     if (!socket) {
-        socket = io(process.env.NEXT_PUBLIC_WS_URL || "http://localhost:3002", {
+        socket = io("https://deprecative-increasedly-shon.ngrok-free.dev/", {
             transports: ["websocket"],
             autoConnect: true,
             reconnection: true,
